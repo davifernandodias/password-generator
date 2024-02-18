c3506980cac060968563aed227e2d04f6130a353
Perkles <davifernandosousadias18@gmail.com>
2024-02-18 12:45:59 -0300

    atribuições

--- gere/geration.py
+++ gere/geration.py
@@ -1,9 +1,9 @@
-import random, string
+import random, string # import biblioteca random and string
 
-tamanho = int(input('escreva quantidade de caracteres: '))
+tamanho = int(input('escreva quantidade de caracteres: ')) # definição de tamanho
 
-char = string.ascii_letters + string.digits + '!#@$'
+char = string.ascii_letters + string.digits + '!#@$' # utilizando biblioteca string com seu metodh  ascii para gerar caracter Upper or Lower
 
-rnd = random.SystemRandom()
+rnd = random.SystemRandom() # chama function SystemRadom para utilizar biblioteca class os.random para gerar numeros aleatorio fornecido pelo sistema operacional
 
-print(''.join(rnd.choice(char) for i in range(tamanho)))
+print(''.join(rnd.choice(char) for i in range(tamanho))) # mostrar senha com join, chice(char) e percorrer pelo tamanho
