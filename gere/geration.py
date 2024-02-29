df52aef4cccc84c50d429dc66690773c40c287bb
Davi Dias <davifernandosousadias18@Gmail.com>
2024-02-29 12:02:44 -0300

    geration.py
    add language EUA

--- gere/geration.py
+++ gere/geration.py
@@ -1,9 +1,9 @@
-import random, string # import biblioteca random and string
+import random, string # import random and string library
 
-tamanho = int(input('escreva quantidade de caracteres: ')) # definição de tamanho
+size = int(input('escreva quantidade de caracteres: ')) # size definition
 
-char = string.ascii_letters + string.digits + '!#@$' # utilizando biblioteca string com seu metodh  ascii para gerar caracter Upper or Lower
+char = string.ascii_letters + string.digits + '!#@$' # using string library with its ascii method to generate Upper or Lower character
 
-rnd = random.SystemRandom() # chama function SystemRadom para utilizar biblioteca class os.random para gerar numeros aleatorio fornecido pelo sistema operacional
+rnd = random.SystemRandom() # calls SystemRadom function to use os.random class library to generate random numbers provided by the operating system
 
-print(''.join(rnd.choice(char) for i in range(tamanho))) # mostrar senha com join, chice(char) e percorrer pelo tamanho
+print(''.join(rnd.choice(char) for i in range(size))) # show password with join, chice(char) and scroll by size
